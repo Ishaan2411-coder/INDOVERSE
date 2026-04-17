c693c1b741dab01a34aaa8551a8932b7c40af369
Ishaan2411-coder <singhscreations2026@gmail.com>
2026-04-17 13:51:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-function scrollToBooks() {
+let selectedRating = 0;
+
+function rate(star) {
+    selectedRating = star;
+    let stars = document.querySelectorAll(".stars span");
+
+    stars.forEach((s, index) => {
+        if (index < star) {
+            s.classList.add("active");
+        } else {
+            s.classList.remove("active");
+        }
+    });
+}
+
+function submitFeedback() {
+    let text = document.getElementById("feedbackText").value;
+
+    if (selectedRating === 0) {
+        alert("Please select rating!");
+        return;
+    }
+
+    document.getElementById("thankYouMsg").innerText =
+        "Thanks for your feedback! ⭐ " + selectedRating + "/5";
+
+    document.getElementById("feedbackText").value = "";
+
+    let stars = document.querySelectorAll(".stars span");
+    stars.forEach(s => s.classList.remove("active"));
+}function scrollToBooks() {
     document.getElementById("books").scrollIntoView({
         behavior: "smooth"
     });
