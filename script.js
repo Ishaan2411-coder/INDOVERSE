88cac6be1b65eac9c2e89523d8f02d22edd1f008
Ishaan2411-coder <singhscreations2026@gmail.com>
2026-04-20 09:18:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
-function showTab(tabId) {
-    document.querySelectorAll(".tab").forEach(tab => {
-        tab.classList.remove("active");
+function scrollToBooks() {
+    document.getElementById("books").scrollIntoView({
+        behavior: "smooth"
     });
-    document.getElementById(tabId).classList.add("active");
-}
-
-let rating = 0;
-
-function rate(num) {
-    rating = num;
-    let stars = document.querySelectorAll(".stars span");
-
-    stars.forEach((s, i) => {
-        if (i < num) {
-            s.classList.add("active");
-        } else {
-            s.classList.remove("active");
-        }
-    });
-}
-
-function submitFeedback() {
-    if (rating === 0) {
-        alert("Select rating!");
-        return;
-    }
-
-    document.getElementById("msg").innerText =
-        "Thanks for your feedback! ⭐ " + rating + "/5";
-
-    document.getElementById("fb").value = "";
 }
