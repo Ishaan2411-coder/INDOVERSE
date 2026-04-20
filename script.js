0ba246e50c1571d482b1df2077864abdbaf25231
Ishaan2411-coder <singhscreations2026@gmail.com>
2026-04-20 09:24:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,30 @@ function scrollToBooks() {
         behavior: "smooth"
     });
 }
+
+let rating = 0;
+
+function rate(num) {
+    rating = num;
+    let stars = document.querySelectorAll(".stars span");
+
+    stars.forEach((s, i) => {
+        if (i < num) {
+            s.classList.add("active");
+        } else {
+            s.classList.remove("active");
+        }
+    });
+}
+
+function submitFeedback() {
+    if (rating === 0) {
+        alert("Select rating!");
+        return;
+    }
+
+    document.getElementById("msg").innerText =
+        "Thanks for your feedback! ⭐ " + rating + "/5";
+
+    document.getElementById("fb").value = "";
+}
